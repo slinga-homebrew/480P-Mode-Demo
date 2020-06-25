0954b2c42e5585e7ae0ad210e4c935c381a43bd3
slinga-homebrew <58894244+slinga-homebrew@users.noreply.github.com>
2020-06-24 23:39:15 -0400

    Progress, still not working
    Added resolution, bug fixes, some SMPC stuff. Missing VDP2 configuration.

--- main.c
+++ main.c
@@ -29,6 +29,45 @@
 #include <jo/jo.h>
 #include "vdp2_cram_data.h"
 
+typedef struct _RESOLUTIONS {
+    unsigned int width;
+    unsigned int height;
+    unsigned int flag;
+} RESOLUTIONS, *PRESOLUTIONS;
+
+// global array at 0x060057a4
+RESOLUTIONS g_Resolutions[] = {
+    { 320, 224, 0x00000000, },
+    { 320, 240, 0x00000010, },
+    { 320, 256, 0x00000020, },
+    { 320, 448, 0x000000c0, },
+    { 320, 480, 0x000000d0, },
+    { 320, 512, 0x000000e0, },
+    { 640, 224, 0x00000002, },
+    { 640, 240, 0x00000012, },
+    { 640, 256, 0x00000022, },
+    { 640, 448, 0x000000c2, },
+    { 640, 480, 0x000000d2, },
+    { 640, 512, 0x000000e2, },
+    { 352, 224, 0x00000001, },
+    { 352, 240, 0x00000011, },
+    { 352, 256, 0x00000021, },
+    { 352, 448, 0x000000c1, },
+    { 352, 480, 0x000000d1, },
+    { 352, 512, 0x000000e1, },
+    { 704, 224, 0x00000003, },
+    { 704, 240, 0x00000013, },
+    { 704, 256, 0x00000023, },
+    { 704, 448, 0x000000c3, },
+    { 704, 480, 0x000000d3, },
+    { 704, 512, 0x000000e3, },
+    { 320, 480, 0x00000004, },
+    { 640, 480, 0x00000006, },
+    { 352, 480, 0x00000005, },
+    { 704, 480, 0x00000007, },
+    { 0xffffffff, 0xffffffff, 0xffffffff, } // end marker
+};
+
 // stdlib function prototypes to keep compiler happy
 void* memcpy(void *dst, const void *src, unsigned int len);
 void *memset(void *s, int c, unsigned int n);
@@ -90,19 +129,29 @@ void copyDataToVDP2CRAMCache()
     *(unsigned short*)(VDP2_CRAM_START + 1) = 0x4000;
 }
 
+volatile unsigned char* SMPC_DDR1 = (void*)0x20100079;
+volatile unsigned char* SMPC_DDR2 = (void*)0x2010007b;
+volatile unsigned char* SMPC_IOSEL1 = (void*)0x2010007d;
+volatile unsigned char* SMPC_EXLE1 = (void*)0x2010007f;
+
 // function 0x06005280
 void initSMPC()
 {
-    // BUGBUG: not implemented
+    *SMPC_DDR1 = 0x60;
+    *SMPC_DDR2 = 0x60;
+    *SMPC_IOSEL1 = 3;
+    *SMPC_EXLE1 = 0;
+
+
     return;
 }
 
 volatile unsigned short* VDP1_TVMR = (unsigned short*)0x25d00000;
 volatile unsigned short* VDP1_FBCR = (unsigned short*)0x25d00002;
 volatile unsigned short* VDP1_PTMR = (unsigned short*)0x25d00004;
-volatile unsigned short* VDP1_EWDR = (unsigned short*)0x25F80006;
-volatile unsigned short* VDP1_EWLR = (unsigned short*)0x25F80008;
-volatile unsigned short* VDP1_EWRR = (unsigned short*)0x25F8000a;
+volatile unsigned short* VDP1_EWDR = (unsigned short*)0x25d00006;
+volatile unsigned short* VDP1_EWLR = (unsigned short*)0x25d00008;
+volatile unsigned short* VDP1_EWRR = (unsigned short*)0x25d0000a;
 
 void initVDP1VRAMCache();
 void setSomethingVDP1VRAMCache();
@@ -158,11 +207,6 @@ void setSomethingVDP1VRAMCache()
 
 void my_draw(void)
 {
-    initVDP2();
-    copyDataToVDP2CRAMCache();
-    initSMPC(); // not implemented
-
-    initVDP1();
     jo_printf(2, 2, "test");
     jo_printf(2, 2, "hello world");
 }
@@ -182,7 +226,7 @@ void printfWrapper(int x,int y, unsigned int flag,char *msg)
   do {
     tempX = x + i;
     i = i + 1;
-    //(*(code *)puVar1)(iVar2,y,flag & 0xff);
+    //printfWrapper2(tempX, y, flag & 0xff, msg[i-1]);
   } while (msg[i] != '\0');
   return;
 }
@@ -192,7 +236,7 @@ int pollVDP2_D0W_8(void)
 {
   do {
 
-  } while ((*VDP2_D0W & 8) != 0);
+  } while ((*VDP2_D0W & 8) == 0);
 
   return 0;
 }
@@ -219,7 +263,7 @@ unsigned int pollVDP2_D0W_4(void)
 // function 0x06004b60
 void setVDP1VRAM(unsigned short a, unsigned short b)
 {
-    unsigned short* vdp1_vram = (VDP1_VRAM_START + (g_VDP1_Global & 0xFFFF)*0x20);
+    unsigned short* vdp1_vram = ((unsigned char*)VDP1_VRAM_START + (g_VDP1_Global & 0xFFFF)*0x20);
 
     vdp1_vram[0] = 9;
     vdp1_vram[1] = 0;
@@ -232,7 +276,7 @@ void setVDP1VRAM(unsigned short a, unsigned short b)
 // function 0x06004ba0
 void setVDP1VRAM_2(unsigned short a, unsigned short b, unsigned short c, unsigned short d)
 {
-    unsigned short* vdp1_vram = (VDP1_VRAM_START + (g_VDP1_Global & 0xFFFF)*0x20);
+    unsigned short* vdp1_vram = ((unsigned char*)VDP1_VRAM_START + (g_VDP1_Global & 0xFFFF)*0x20);
 
     vdp1_vram[0] = 8;
     vdp1_vram[1] = 0;
@@ -247,7 +291,7 @@ void setVDP1VRAM_2(unsigned short a, unsigned short b, unsigned short c, unsigne
 // function 0x06004be0
 void setVDP1VRAM_3(unsigned short a, unsigned short b)
 {
-    unsigned short* vdp1_vram = (VDP1_VRAM_START + (g_VDP1_Global & 0xFFFF)*0x20);
+    unsigned short* vdp1_vram = ((unsigned char*)VDP1_VRAM_START + (g_VDP1_Global & 0xFFFF)*0x20);
 
     vdp1_vram[0] = 10;
     vdp1_vram[1] = 0;
@@ -292,32 +336,101 @@ void setVDP1VRAM_5()
     g_VDP1_Global++;
 }
 
+volatile unsigned char* SMPC_PDR1 = (void*)0x20100075;
+
+// function 0x060052b0
+void readSMPC(unsigned int unused, unsigned short* outData)
+{
+    *SMPC_PDR1 = 0x60;
+    unsigned char val1, val2, val3;
+    unsigned short val4;
+    unsigned short val5;
+
+    // BUGBUG: won't the compiler just optimize this out??
+    for(int i = 0x10; i > 0; i--)
+    {
+
+    }
+
+    val1 = *SMPC_PDR1;
+    *SMPC_PDR1= 0x40;
+
+    // BUGBUG: won't the compiler just optimize this out??
+    for(int i = 0x10; i > 0; i--)
+    {
+
+    }
+
+    val2 = *SMPC_PDR1;
+    *SMPC_PDR1= 0x20;
+
+    // BUGBUG: won't the compiler just optimize this out??
+    for(int i = 0x10; i > 0; i--)
+    {
+
+    }
+
+    val3 = *SMPC_PDR1;
+    *SMPC_PDR1= 0x0;
+
+    // BUGBUG: won't the compiler just optimize this out??
+    for(int i = 0x10; i > 0; i--)
+    {
+
+    }
+
+    val5 = (*SMPC_PDR1 & 0xF) |
+            (val3 & 0xf) << 4 |
+            (val2 & 0xf) << 8 |
+            ((val1 & 0x8 << 0xc)) ^ 0x8FFF;
+
+    val4 = *outData;
+
+    outData[0] = val5;
+    outData[1] = val4;
+    outData[2] = val5 & (val5 ^ val4);
+
+    return;
+}
+
+void printMenu(int param_1)
+{
+
+}
+
 
 void jo_main(void)
 {
-    //jo_core_init(JO_COLOR_Black);
+    unsigned short smpcData[3];
 
-    //jo_core_add_callback(my_draw);
+    jo_core_init(JO_COLOR_Black);
+    /*
+
+    jo_core_add_callback(my_draw);
+    jo_core_run();
+    */
+
     initVDP2();
     copyDataToVDP2CRAMCache();
     initSMPC(); // not implemented
 
     initVDP1();
-    // printfWrapper(2, 1, 0x0f, "BG0123")
+    printfWrapper(2, 1, 0x0f, "BG0123");
 
     *VDP2_D0R = 0x8000;
 
-    // setSMPCRegisters()
+    readSMPC(0, smpcData);
 
     g_currentIndex = 0;
 
-    // print menu
-    // setSMPCRegisters()
+    // printMenu()
+
 
     pollVDP2_D0W_8();
     pollVDP2_D0W_8_2();
 
     *VDP1_FBCR = 1;
+    readSMPC(0, smpcData);
 
     unsigned short someVar = 0;
     unsigned short someVar2 = 2;
@@ -325,6 +438,8 @@ void jo_main(void)
     while(1)
     {
         // poll controller for inputs and change globals
+        // if the user presses C, increment the resolution
+        // and check if width == -1 meaning we are at the end of the array
 
         *VDP2_PPAF = someVar;
         *VDP2_PPD = someVar2;
@@ -333,6 +448,9 @@ void jo_main(void)
         setVDP1VRAM(0xA0, 0x70);
         setVDP1VRAM_2(0, 0, 0xA0, 0x70);
         setVDP1VRAM_3(0, 0);
+
+        // set some global to 0??
+
         setVDP1VRAM_4(0x9f, 0, 0x9f, 0x6f, 0, 0x6f, 0, 0, 0xfc1f);
         setVDP1VRAM_5();
 
@@ -341,10 +459,11 @@ void jo_main(void)
 
         *VDP1_FBCR = 1;
 
-        // setSMPCRegisters()
+        readSMPC(0, smpcData);
 
     }
 
-    //jo_core_run();
+
+
 
 }
